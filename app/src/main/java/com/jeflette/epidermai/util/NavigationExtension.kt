9e890b002ef75fe3f894881f74b79f1223086b15
dionpouw <aldion.sumampouw@gmail.com>
2023-05-29 23:20:37 +0800

    na

---
+++ app/src/main/java/com/jeflette/epidermai/util/NavigationExtension.kt
@@ -0,0 +1,8 @@
+package com.jeflette.epidermai.util
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+
+fun NavController.safeNavigate(direction: NavDirections) {
+    currentDestination?.getAction(direction.actionId)?.run { navigate(direction) }
+}
