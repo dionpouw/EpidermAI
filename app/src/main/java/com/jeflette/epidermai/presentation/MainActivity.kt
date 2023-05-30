911193e381f25f644a56b07f95fb3226e69117c3
dionpouw <aldion.sumampouw@gmail.com>
2023-05-30 23:38:07 +0800

    setup navigation bar logic

--- app/src/main/java/com/jeflette/epidermai/presentation/MainActivity.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.jeflette.epidermai.presentation
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jeflette.epidermai.R
 import com.jeflette.epidermai.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +13,24 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding
 
+    private val navHostController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        binding?.navView?.setupWithNavController(navHostController)
+
+        navHostController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment || destination.id == R.id.registerFragment) {
+                binding?.navView?.visibility = View.GONE
+            } else {
+                binding?.navView?.visibility = View.VISIBLE
+            }
+        }
     }
 
     override fun onDestroy() {
