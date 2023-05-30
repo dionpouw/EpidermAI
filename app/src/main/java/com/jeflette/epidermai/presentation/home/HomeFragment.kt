858a4bee9ae9b8782531fcbcfddb06e8cf4da6eb
dionpouw <aldion.sumampouw@gmail.com>
2023-05-30 23:37:24 +0800

    add logout button and its logic

--- app/src/main/java/com/jeflette/epidermai/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/home/HomeFragment.kt
@@ -5,18 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.jeflette.epidermai.databinding.FragmentHomeBinding
+import com.jeflette.epidermai.util.safeNavigate
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+
+        binding?.apply {
+            logoutButton.setOnClickListener {
+                logout()
+            }
+        }
+    }
+
+    private fun logout() {
+        auth.signOut()
+        findNavController().safeNavigate(HomeFragmentDirections.actionHomeFragmentToLoginFragment())
     }
 
     override fun onDetach() {
