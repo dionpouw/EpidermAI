2f2e6eea0e606706a98920eb3732384bd8a2d5e1
dionpouw <aldion.sumampouw@gmail.com>
2023-05-30 23:39:03 +0800

    history fragment init

---
+++ app/src/main/java/com/jeflette/epidermai/presentation/history/HistoryFragment.kt
@@ -0,0 +1,28 @@
+package com.jeflette.epidermai.presentation.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jeflette.epidermai.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private var _binding: FragmentHistoryBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        _binding = null
+    }
+
+}
+
---
+++ app/src/main/java/com/jeflette/epidermai/presentation/history/HistoryViewModel.kt
@@ -0,0 +1,7 @@
+package com.jeflette.epidermai.presentation.history
+
+import androidx.lifecycle.ViewModel
+
+class HistoryViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
