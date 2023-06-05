4decd43ce1bcf79ec7395241886e5e19ed0c03a0
dionpouw <aldion.sumampouw@gmail.com>
2023-06-05 22:14:15 +0800

    add recyclerview logic

--- app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineFragment.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import com.jeflette.epidermai.databinding.FragmentMedicineBinding
+import com.jeflette.epidermai.presentation.adapter.CardItemAdapter
+import com.jeflette.epidermai.util.provideDummyMedicine
 
 class MedicineFragment : Fragment() {
 
@@ -17,6 +20,26 @@ class MedicineFragment : Fragment() {
     ): View? {
         _binding = FragmentMedicineBinding.inflate(inflater, container, false)
         return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val diseaseAdapter = CardItemAdapter()
+
+        binding?.apply {
+            medicineRecyclerView.apply {
+                adapter = diseaseAdapter
+                layoutManager = GridLayoutManager(context, 2)
+                setHasFixedSize(false)
+            }
+        }
+        populateDiseaseList(diseaseAdapter)
+    }
+
+    private fun populateDiseaseList(diseaseAdapter: CardItemAdapter) {
+        val data = provideDummyMedicine()
+        diseaseAdapter.addCardItemList(data)
     }
 
     override fun onDetach() {
