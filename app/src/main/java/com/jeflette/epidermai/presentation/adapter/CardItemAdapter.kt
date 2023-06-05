65d2167c88b4c5ef49885ddf21d9dd5c0540e37a
dionpouw <aldion.sumampouw@gmail.com>
2023-06-05 22:14:49 +0800

    carditem adapter

---
+++ app/src/main/java/com/jeflette/epidermai/presentation/adapter/CardItemAdapter.kt
@@ -0,0 +1,48 @@
+package com.jeflette.epidermai.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jeflette.epidermai.databinding.CardItemBinding
+import com.jeflette.epidermai.model.CommonItem
+import com.jeflette.epidermai.presentation.home.HomeFragmentDirections
+
+class CardItemAdapter : RecyclerView.Adapter<CardItemAdapter.ViewHolder>() {
+
+    private val commonItemList = mutableListOf<CommonItem>()
+
+    fun addCardItemList(list: List<CommonItem>) {
+        this.commonItemList.clear()
+        commonItemList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    inner class ViewHolder(private val itemBinding: CardItemBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun binding(commonItem: CommonItem) {
+            Glide.with(itemView.context).load(commonItem.image).into(itemBinding.cardImage)
+            itemBinding.cardName.text = commonItem.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        CardItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+
+    override fun getItemCount(): Int = commonItemList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding(commonItemList[position])
+        holder.itemView.setOnClickListener {
+            val actionHomeFragmentToDiseaseDetailFragment =
+                HomeFragmentDirections.actionHomeFragmentToDiseaseDetailFragment(commonItemList[position].id)
+            holder.itemView.findNavController()
+                .navigate(actionHomeFragmentToDiseaseDetailFragment)
+        }
+    }
+}
