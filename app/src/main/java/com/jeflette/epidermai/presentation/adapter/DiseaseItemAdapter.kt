bdae474c6158d59d902b8bd3d3efc93528d509a4
dionpouw <aldion.sumampouw@gmail.com>
2023-06-05 22:16:24 +0800

    Disease fragment adapter

--- app/src/main/java/com/jeflette/epidermai/presentation/adapter/DiseaseItemAdapter.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/adapter/DiseaseItemAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jeflette.epidermai.databinding.DiseaseItemBinding
 import com.jeflette.epidermai.model.Disease
+import com.jeflette.epidermai.presentation.disease.DiseaseFragmentDirections
 
 class DiseaseItemAdapter : RecyclerView.Adapter<DiseaseItemAdapter.ViewHolder>() {
 
@@ -20,8 +21,9 @@ class DiseaseItemAdapter : RecyclerView.Adapter<DiseaseItemAdapter.ViewHolder>()
     inner class ViewHolder(private val itemBinding: DiseaseItemBinding) :
         RecyclerView.ViewHolder(itemBinding.root) {
         fun binding(disease: Disease) {
-            Glide.with(itemView.context).load(disease.image).into(itemBinding.diseaseImage)
-            itemBinding.diseaseName.text = disease.name
+            Glide.with(itemView.context).load(disease.diseaseItem.image)
+                .into(itemBinding.diseaseImage)
+            itemBinding.diseaseName.text = disease.diseaseItem.name
             itemBinding.diseaseDesc.text = disease.description
         }
     }
@@ -32,10 +34,13 @@ class DiseaseItemAdapter : RecyclerView.Adapter<DiseaseItemAdapter.ViewHolder>()
         )
     )
 
-
     override fun getItemCount(): Int = diseaseList.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding(diseaseList[position])
+        holder.itemView.setOnClickListener {
+            val actionDiseaseFragmentToDiseaseDetailFragment =
+                DiseaseFragmentDirections.actionDiseaseFragmentToDiseaseDetailFragment(diseaseList[position].diseaseItem.id)
+        }
     }
 }
