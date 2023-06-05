3d1a2f22afcc253d03df53151c393956ccf8cedc
dionpouw <aldion.sumampouw@gmail.com>
2023-06-05 22:15:13 +0800

    Camera fragment

--- app/src/main/java/com/jeflette/epidermai/presentation/camera/CameraFragment.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/camera/CameraFragment.kt
@@ -1,21 +1,21 @@
-package com.jeflette.epidermai.presentation.detection
+package com.jeflette.epidermai.presentation.camera
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.jeflette.epidermai.databinding.FragmentDetectionBinding
+import com.jeflette.epidermai.databinding.FragmentCameraBinding
 
-class DetectionFragment : Fragment() {
+class CameraFragment : Fragment() {
 
-    private var _binding: FragmentDetectionBinding? = null
+    private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentDetectionBinding.inflate(inflater, container, false)
+        _binding = FragmentCameraBinding.inflate(inflater, container, false)
         return binding?.root
     }
 
---
+++ app/src/main/java/com/jeflette/epidermai/presentation/camera/CameraViewModel.kt
@@ -0,0 +1,7 @@
+package com.jeflette.epidermai.presentation.camera
+
+import androidx.lifecycle.ViewModel
+
+class CameraViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
