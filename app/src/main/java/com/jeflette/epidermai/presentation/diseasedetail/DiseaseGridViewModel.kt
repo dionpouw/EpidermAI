f86926a624c9b0591c1793cd17f5afc2ae0a69d7
dionpouw <aldion.sumampouw@gmail.com>
2023-06-10 21:00:01 +0800

    remove unused fragment

--- app/src/main/java/com/jeflette/epidermai/presentation/diseasedetail/DiseaseGridViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.jeflette.epidermai.presentation.diseasedetail
-
-import androidx.lifecycle.ViewModel
-
-class DiseaseGridViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
