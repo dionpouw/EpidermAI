1349a4a3794854730694adb9b8f2d15591e77547
dionpouw <aldion.sumampouw@gmail.com>
2023-06-11 22:21:53 +0800

    Add dummy data with image link

--- app/src/main/java/com/jeflette/epidermai/util/DummyData.kt
+++ app/src/main/java/com/jeflette/epidermai/util/DummyData.kt
@@ -1,6 +1,5 @@
 package com.jeflette.epidermai.util
 
-import com.jeflette.epidermai.R
 import com.jeflette.epidermai.model.CommonItem
 import com.jeflette.epidermai.model.Disease
 
@@ -8,59 +7,174 @@ fun provideDummyDisease(): List<Disease> {
     return listOf<Disease>(
         Disease(
             CommonItem(
-                1, R.drawable.acne, "Acne"
-            ), "Jerawat", listOf(
+                1,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/acne_rosacea.jpg",
+                "Acne & Rosacea"
+            ),
+            "Jerawat adalah masalah kulit yang terjadi ketika pori-pori kulit, tepatnya folikel rambut, tersumbat oleh kotoran, debu, minyak, atau sel kulit mati. Akibatnya, terjadi peradangan pada pori-pori tersebut dan bisa juga disertai infeksi. Jerawat sering muncul di wajah, leher, punggung, atau dada. Rosacea adalah penyakit kulit wajah yang ditandai dengan kulit kemerahan disertai bintik-bintik menyerupai jerawat. Kondisi ini juga dapat menyebabkan kulit wajah menebal dan pembuluh darah di wajah membengkak.\n" + "\n" + "Rosacea dapat terjadi pada siapa saja, tetapi umumnya menyerang wanita usia paruh baya yang berkulit terang. Gejala rosacea bisa hilang timbul dan umumnya  berlangsung selama beberapa minggu atau beberapa bulan.",
+            listOf(
                 CommonItem(
-                    11, R.drawable.benzasil, "Benzasil"
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Zelface.jpeg",
+                    "Zelface"
                 ), CommonItem(
-                    12, R.drawable.lumiquin, "Lumiquin"
+                    12,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Metronidazole.png",
+                    "Metronidazole"
                 ), CommonItem(
-                    13, R.drawable.erysanbe, "Erysanbe"
-                ), CommonItem(
-                    14, R.drawable.clindamycin, "Clindamycin"
+                    13,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Retinoid%20Acid.jpg",
+                    "Retinoid Acid"
                 )
             )
         ), Disease(
             CommonItem(
-                1, R.drawable.acne, "Acne"
-            ), "Jerawat", listOf(
+                2,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/berign_kerastosis.jpg",
+                "Benign keratosis"
+            ),
+            "Keratosis seboroik adalah benjolan seperti kutil yang tumbuh di permukaan kulit dan bisa bertambah seiring waktu. Kondisi ini lebih sering terjadi pada orang dewasa, terutama yang lanjut usia.\n" + "\n" + "Keratosis seboroik dapat tumbuh di permukaan kulit mana pun, kecuali di telapak tangan dan kaki. Bagian tubuh yang sering menjadi lokasi kemunculan keratosis seboroik adalah wajah, dada, bahu, punggung, dan lipatan kulit.",
+            listOf(
                 CommonItem(
-                    11, R.drawable.benzasil, "Benzasil"
-                ), CommonItem(
-                    12, R.drawable.lumiquin, "Lumiquin"
-                ), CommonItem(
-                    13, R.drawable.erysanbe, "Erysanbe"
-                ), CommonItem(
-                    14, R.drawable.clindamycin, "Clindamycin"
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Imiquimod.jpg",
+                    "Imiquimod "
                 )
             )
         ), Disease(
             CommonItem(
-                1, R.drawable.acne, "Acne"
-            ), "Jerawat", listOf(
+                3,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/chickenpox.jpg",
+                "Chickenpox"
+            ),
+            "Cacar air dalam istilah medis dikenal dengan varicella. Penyebabnya adalah virus Varicella zoster. Virus ini mudah menular melalui percikan ludah, atau kontak langsung dengan cairan yang berasal dari ruam.\n" + "\n" + "Penyakit ini lebih rentan menyerang anak-anak usia di bawah 12 tahun. Beberapa faktor lain yang dapat meningkatkan risiko terkena cacar air adalah belum pernah menderita cacar air sebelumnya dan belum menerima vaksin cacar air.\n" + "\n" + "Gejala cacar air muncul 10−21 hari setelah terpapar virus tersebut. Gejala utama cacar air adalah bentol-bentol kemerahan dan berisi cairan di wajah, dada, atau punggung, yang dapat menyebar ke seluruh bagian tubuh. Sekitar 2 hari sebelum bentol muncul, cacar air ditandai dengan keluhan lain, seperti:\n" + "\n" + "Demam\n" + "Sakit kepala\n" + "Sakit tenggorokan\n" + "Lemas\n" + "Hilang nafsu makan\n" + "Penyakit ini biasanya sembuh setelah 5−10 hari jika daya tahan tubuh penderitanya baik.",
+            listOf(
                 CommonItem(
-                    11, R.drawable.benzasil, "Benzasil"
-                ), CommonItem(
-                    12, R.drawable.lumiquin, "Lumiquin"
-                ), CommonItem(
-                    13, R.drawable.erysanbe, "Erysanbe"
-                ), CommonItem(
-                    14, R.drawable.clindamycin, "Clindamycin"
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/acyclovir.jpeg",
+                    "Acyclovir"
                 )
             )
         ), Disease(
             CommonItem(
-                1, R.drawable.acne, "Acne"
-            ), "Jerawat", listOf(
+                4,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/dermatofibroma.jpg",
+                "Dermatofibroma"
+            ),
+            "Dermatofibroma adalah suatu kondisi kulit yang tampak seperti pertumbuhan jaringan yang berukuran kecil dan bulat. Kulit memiliki lapisan yang berbeda, diantaranya sel-sel lemak subkutan, dermis, dan epidermis. Dermatofit adalah pertumbuhan jaringan yang terjadi akibat sel pada lapisan kedua kulit (dermis) tumbuh secara tidak normal.\n" + "\n" + "Dermatofibroma dianggap sebagai tumor jinak di kulit yang dapat terjadi pada semua orang. Walaupun demikian, dermatofibroma bersifat jinak (tidak bersifat kanker) dan bukan merupakan kondisi kulit yang perlu dikhawatirkan.",
+            listOf(
                 CommonItem(
-                    11, R.drawable.benzasil, "Benzasil"
-                ), CommonItem(
-                    12, R.drawable.lumiquin, "Lumiquin"
-                ), CommonItem(
-                    13, R.drawable.erysanbe, "Erysanbe"
-                ), CommonItem(
-                    14, R.drawable.clindamycin, "Clindamycin"
+                    51,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/dermatofibroma.jpg",
+                    "Dermatofibroma"
+                ),
+            ),
+        ), Disease(
+            CommonItem(
+                5,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/distal_subungual_onychomycosis.png",
+                "Distal Subungual Onychomycosis"
+            ),
+            "Onikomikosis merupakan infeksi jamur pada kuku yang disebabkan oleh jamur dermatofita, ragi, atau kapang. Manifestasi klinis onikomikosis antara lain perubahan warna kuku atau diskromasi, penebalan kuku, onikolisis, dan debris subungual. Onikomikosis mempengaruhi kualitas hidup penderitanya, antara lain nyeri, kesulitan memakai sepatu dan melakukan pekerjaan, infeksi sekunder, hingga masalah penampilan secara kosmetik. Oleh karena itu penderita onikomikosis berusaha mengobati keluhannya hingga sembuh. ",
+            listOf(
+                CommonItem(
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ketokonazole.jpg",
+                    "Ketokonazole"
+                ),
+                CommonItem(
+                    12,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/miconazole.jpeg",
+                    "Miconazole"
+                ),
+                CommonItem(
+                    13,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Ciclopirox.webp",
+                    "Ciclopirox"
+                ),
+            )
+        ), Disease(
+            CommonItem(
+                6,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/measles.jpg",
+                "Measles"
+            ),
+            "Campak atau measles adalah penyakit akibat infeksi virus yang ditandai dengan demam, sakit tenggorokan, dan ruam di seluruh tubuh. Infeksi campak berawal dari saluran pernapasan yang kemudian menular melalui percikan air liur.\n" + "\n" + "Gejala awal campak adalah sakit tenggorokan, mata berair dan kemerahan (konjungtivitis), dan bintik putih di dalam mulut. Ruam yang muncul mulanya berupa bintik-bintik merah kecil, kemudian menyatu hingga ukurannya tampak lebih besar.",
+            listOf(
+                CommonItem(
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ibuprofen.jpg",
+                    "Ibuprofen"
+                ),
+                CommonItem(
+                    12,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/paracetamol.jpg",
+                    "Paracetamol"
+                ),
+            )
+        ), Disease(
+            CommonItem(
+                7,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/melanocytic_nevus.jpg",
+                "Melanocytic nevus"
+            ),
+            "merupakan penyakit langka yang timbul saat lahir akibat sel melanosit tumbuh berlebih. Lesi pada GCMN sering ditemukan pada area punggung dan paha. Gambaran lesi berupa bercak hiperpigmentasi dengan ukuran lebih dari 20 cm dan terdapat rambut. Kondisi ini sering dihubungkan dengan neurokutaneus melanositosis dan melanoma maligna. Risiko neurokutaneus melanositosis meningkat signifikans bila lesi giant nevi terdapat pada regio kranial atau garis midline atau disertai lesi satelit. Rerata kumulatif risiko terjadinya melanoma dalam 5 tahun sebesar 4,5%. Pendekatan terapi masih menjadi tantangan dan bersifat individual bergantung pada usia, lokasi lesi, ukuran, risiko melanoma dan kemungkinan kelainan fungsi akibat tindakan invasif yang dilakukan serta dampak fisiologis terhadap luka pasca tindakan.",
+            listOf(
+                CommonItem(
+                    11, "", ""
                 )
+            )
+        ), Disease(
+            CommonItem(
+                8,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/melanoma.jpg",
+                "Melanoma"
+            ),
+            "Kanker kulit melanoma adalah kanker kulit yang berkembang dari melanosit. Selain di kulit, melanoma juga dapat muncul di mata. Bahkan, pada kasus yang jarang terjadi, melanoma bisa tumbuh di dalam hidung atau tenggorokan.\n" + "\n" + "Melanosit adalah sel pigmen kulit yang berfungsi menghasilkan melanin, yaitu pigmen yang menghasilkan warna kulit manusia. Melanin inilah yang bekerja menyerap sinar ultraviolet dan melindungi kulit dari kerusakan.",
+            listOf(
+                CommonItem(
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/radioterapi.jpg",
+                    "Radiasi"
+                ),
+                CommonItem(
+                    12,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/kemoterapi.jpg",
+                    "Kemoterapi"
+                ),
+            )
+        ), Disease(
+            CommonItem(
+                9,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/monkeypox.jpg",
+                "Monkeypox"
+            ),
+            "Cacar monyet adalah infeksi virus yang ditandai dengan bintil bernanah di kulit. Cacar monyet atau monkeypox pertama kali muncul di negara Republik Demokratik Kongo pada tahun 1970.\n" + "\n" + "Pada awalnya, penyakit cacar monyet memiliki gejala yang serupa dengan cacar air, yaitu bintil berair. Seiring perkembangan penyakit, bintil berair berubah menjadi bernanah dan menimbulkan benjolan di leher, ketiak, atau selangkangan akibat pembengkakan kelenjar getah bening.",
+            listOf(
+                CommonItem(
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/acyclovir.jpeg",
+                    "Acyclovir"
+                )
+            )
+        ), Disease(
+            CommonItem(
+                10,
+                "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/vascular_lesion.jpg",
+                "Vascular lesion"
+            ),
+            "Lesi vaskular adalah kelainan yang relatif umum pada kulit dan jaringan di bawahnya, lebih dikenal sebagai tanda lahir. Ada tiga kategori utama lesi vaskular: Hemangioma, Malformasi Vaskular, dan Granuloma Piogenik. Meskipun tanda lahir ini terkadang terlihat serupa, masing-masing berbeda dalam hal asal dan perawatan yang diperlukan.\n" + "\n" + "Hemangioma adalah jenis lesi vaskular yang paling umum pada anak-anak. Hemangioma adalah tumor jinak, atau non-kanker, pada sel-sel yang melapisi pembuluh darah. Mereka biasanya muncul pada atau segera setelah lahir sebagai area samar dari perubahan warna merah muda kemerah-merahan pada kulit dan kemudian dengan cepat mengalami periode pertumbuhan yang cepat.",
+            listOf(
+                CommonItem(
+                    11,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/vbeam.png",
+                    "Pulsed-dye laser"
+                ),
+                CommonItem(
+                    12,
+                    "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ipl.jpeg",
+                    "Intense pulsed light"
+                ),
             )
         )
     )
@@ -70,13 +184,74 @@ fun provideDummyDisease(): List<Disease> {
 fun provideDummyMedicine(): List<CommonItem> {
     return listOf(
         CommonItem(
-            11, R.drawable.benzasil, "Benzasil"
-        ), CommonItem(
-            12, R.drawable.lumiquin, "Lumiquin"
-        ), CommonItem(
-            13, R.drawable.erysanbe, "Erysanbe"
-        ), CommonItem(
-            14, R.drawable.clindamycin, "Clindamycin"
-        )
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Zelface.jpeg",
+            "Zelface"
+        ),
+        CommonItem(
+            12,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Metronidazole.png",
+            "Metronidazole"
+        ),
+        CommonItem(
+            13,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Retinoid%20Acid.jpg",
+            "Retinoid Acid"
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Imiquimod.jpg",
+            "Imiquimod "
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/acyclovir.jpeg",
+            "Acyclovir"
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/vbeam.png",
+            "Pulsed-dye laser"
+        ),
+        CommonItem(
+            12,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ipl.jpeg",
+            "Intense pulsed light"
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/radioterapi.jpg",
+            "Radiasi"
+        ),
+        CommonItem(
+            12,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/kemoterapi.jpg",
+            "Kemoterapi"
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ibuprofen.jpg",
+            "Ibuprofen"
+        ),
+        CommonItem(
+            12,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/paracetamol.jpg",
+            "Paracetamol"
+        ),
+        CommonItem(
+            11,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/ketokonazole.jpg",
+            "Ketokonazole"
+        ),
+        CommonItem(
+            12,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/miconazole.jpeg",
+            "Miconazole"
+        ),
+        CommonItem(
+            13,
+            "https://raw.githubusercontent.com/dionpouw/CapstoneFiles/main/Ciclopirox.webp",
+            "Ciclopirox"
+        ),
     )
 }
