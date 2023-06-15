21ff9cc01f9064dcda4ba033ca54356c73d55d4e
dionpouw <aldion.sumampouw@gmail.com>
2023-06-15 17:19:58 +0800

    medicine fragment

--- app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineFragment.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.GridLayoutManager
+import com.jeflette.epidermai.data.local.model.CommonItem
 import com.jeflette.epidermai.databinding.FragmentMedicineBinding
 import com.jeflette.epidermai.presentation.adapter.CardItemAdapter
-import com.jeflette.epidermai.util.provideDummyMedicine
+import com.jeflette.epidermai.util.Resource
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MedicineFragment : Fragment() {
 
+    private val viewModel: MedicineViewModel by viewModels()
     private var _binding: FragmentMedicineBinding? = null
     private val binding get() = _binding
 
@@ -26,6 +36,7 @@ class MedicineFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val diseaseAdapter = CardItemAdapter()
+        viewModel.getAllDiseases()
 
         binding?.apply {
             medicineRecyclerView.apply {
@@ -34,13 +45,56 @@ class MedicineFragment : Fragment() {
                 setHasFixedSize(false)
             }
         }
-        populateDiseaseList(diseaseAdapter)
+        populateDiseaseList(binding, diseaseAdapter)
     }
 
-    private fun populateDiseaseList(diseaseAdapter: CardItemAdapter) {
-        val data = provideDummyMedicine()
-        diseaseAdapter.addCardItemList(data)
+    private fun populateDiseaseList(
+        binding: FragmentMedicineBinding?, diseaseAdapter: CardItemAdapter
+    ) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.diseaseResult.collectLatest { medicineResult ->
+                    when (medicineResult) {
+                        is Resource.Error -> {}
+                        is Resource.Loading -> hideVisibility(binding)
+                        is Resource.Success -> {
+                            showVisibility(binding)
+                            medicineResult.data?.let { data ->
+                                val medicineData = mutableListOf<CommonItem>()
+                                for (medicines in data.data!!) {
+                                    val realData = CommonItem(
+                                        id = medicines.mdcId,
+                                        name = medicines.mdcName,
+                                        image = medicines.mdcImg,
+                                        desc = medicines.mdcDesc
+                                    )
+                                    medicineData.add(realData)
+                                }
+                                diseaseAdapter.addCardItemList(medicineData)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    private fun hideVisibility(binding: FragmentMedicineBinding?) {
+        binding?.apply {
+            progressBar.visibility = View.VISIBLE
+            searchView.visibility = View.GONE
+            medicineRecyclerView.visibility = View.GONE
+        }
+    }
+
+    private fun showVisibility(binding: FragmentMedicineBinding?) {
+        binding?.apply {
+            progressBar.visibility = View.GONE
+            searchView.visibility = View.VISIBLE
+            medicineRecyclerView.visibility = View.VISIBLE
+        }
+    }
+
 
     override fun onDetach() {
         super.onDetach()
--- app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineViewModel.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/medicine/MedicineViewModel.kt
@@ -1,7 +1,32 @@
 package com.jeflette.epidermai.presentation.medicine
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jeflette.epidermai.data.EpidermAIRepository
+import com.jeflette.epidermai.data.remote.responses.MedicinesResponse
+import com.jeflette.epidermai.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MedicineViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class MedicineViewModel @Inject constructor(
+    private val repository: EpidermAIRepository
+) : ViewModel() {
+
+    private var _medicineResult: MutableStateFlow<Resource<MedicinesResponse?>> =
+        MutableStateFlow(Resource.Loading())
+
+    val diseaseResult = _medicineResult.asStateFlow()
+
+    fun getAllDiseases() {
+        viewModelScope.launch {
+            repository.getAllMedicines().collectLatest {
+                _medicineResult.value = it
+            }
+        }
+    }
 }
