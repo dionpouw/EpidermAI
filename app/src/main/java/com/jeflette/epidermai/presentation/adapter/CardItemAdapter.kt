27d56623d9dcd97f2890498f94fec35431ed60bc
dionpouw <aldion.sumampouw@gmail.com>
2023-06-15 17:14:02 +0800

    added nav directions

--- app/src/main/java/com/jeflette/epidermai/presentation/adapter/CardItemAdapter.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/adapter/CardItemAdapter.kt
@@ -2,13 +2,19 @@ package com.jeflette.epidermai.presentation.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jeflette.epidermai.R
+import com.jeflette.epidermai.data.local.model.CommonItem
+import com.jeflette.epidermai.data.remote.responses.DiseasesDetailResponse
 import com.jeflette.epidermai.databinding.CardItemBinding
-import com.jeflette.epidermai.model.CommonItem
+import com.jeflette.epidermai.presentation.diseasedetail.DiseaseDetailFragment
+import com.jeflette.epidermai.presentation.diseasedetail.DiseaseDetailFragmentDirections
 import com.jeflette.epidermai.presentation.home.HomeFragmentDirections
+import com.jeflette.epidermai.presentation.medicine.MedicineFragmentDirections
+import com.jeflette.epidermai.util.safeNavigate
+import java.util.Locale
 
 class CardItemAdapter : RecyclerView.Adapter<CardItemAdapter.ViewHolder>() {
 
@@ -40,10 +46,31 @@ class CardItemAdapter : RecyclerView.Adapter<CardItemAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.binding(commonItemList[position])
         holder.itemView.setOnClickListener {
-            val actionHomeFragmentToDiseaseDetailFragment =
-                HomeFragmentDirections.actionHomeFragmentToDiseaseDetailFragment(commonItemList[position].name)
-            holder.itemView.findNavController()
-                .navigate(actionHomeFragmentToDiseaseDetailFragment)
+            findNavController(it).currentDestination?.id.let { id ->
+                when (id) {
+                    R.id.homeFragment -> {
+                        findNavController(it).safeNavigate(
+                            HomeFragmentDirections.actionHomeFragmentToDiseaseDetailFragment(
+                                commonItemList[position].name.lowercase(Locale.ROOT)
+                            )
+                        )
+                    }
+                    R.id.medicineFragment -> {
+                        findNavController(it).safeNavigate(
+                            MedicineFragmentDirections.actionMedicineFragmentToMedicineDetailFragment(
+                                commonItemList[position].name.lowercase(Locale.ROOT)
+                            )
+                        )
+                    }
+                    R.id.diseaseDetailFragment ->{
+                        findNavController(it).safeNavigate(
+                            DiseaseDetailFragmentDirections.actionDiseaseDetailFragmentToMedicineDetailFragment(
+                                commonItemList[position].name.lowercase(Locale.ROOT)
+                            )
+                        )
+                    }
+                }
+            }
         }
     }
 }
