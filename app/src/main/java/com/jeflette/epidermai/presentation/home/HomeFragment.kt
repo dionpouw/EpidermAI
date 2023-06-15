0b1b247683be5021a2876f9fa2f241984019e729
dionpouw <aldion.sumampouw@gmail.com>
2023-06-15 17:19:43 +0800

    home fragment

--- app/src/main/java/com/jeflette/epidermai/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/home/HomeFragment.kt
@@ -6,20 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.jeflette.epidermai.data.local.model.CommonItem
 import com.jeflette.epidermai.databinding.FragmentHomeBinding
-import com.jeflette.epidermai.model.CommonItem
+import com.jeflette.epidermai.presentation.adapter.BlogCardAdapter
 import com.jeflette.epidermai.presentation.adapter.CardItemAdapter
-import com.jeflette.epidermai.util.provideDummyDisease
-import com.jeflette.epidermai.util.provideDummyMedicine
+import com.jeflette.epidermai.util.Resource
 import com.jeflette.epidermai.util.safeNavigate
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    private val viewModel: HomeViewModel by viewModels()
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
     private lateinit var auth: FirebaseAuth
@@ -37,7 +46,10 @@ class HomeFragment : Fragment() {
         auth = Firebase.auth
 
         val diseaseAdapter = CardItemAdapter()
-        val blogAdapter = CardItemAdapter()
+        val blogAdapter = BlogCardAdapter()
+
+        viewModel.getBlog()
+        viewModel.getAllDiseases()
 
         binding?.apply {
             logoutButton.setOnClickListener {
@@ -61,7 +73,8 @@ class HomeFragment : Fragment() {
         populateDiseaseList(diseaseAdapter)
         populateBlogList(blogAdapter)
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner,
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
             object : OnBackPressedCallback(true) {
                 override fun handleOnBackPressed() {
                     requireActivity().finish()
@@ -69,19 +82,64 @@ class HomeFragment : Fragment() {
             })
     }
 
-    private fun populateBlogList(blogAdapter: CardItemAdapter) =
-        blogAdapter.addCardItemList(provideDummyMedicine())
+    private fun populateBlogList(blogAdapter: BlogCardAdapter) {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.blogResult.collectLatest { blogResult ->
+                    when (blogResult) {
+                        is Resource.Loading -> {
+                            binding?.blogProgressBar?.visibility = View.VISIBLE
+                        }
 
+                        is Resource.Error -> {
+                            binding?.blogProgressBar?.visibility = View.GONE
+                        }
+
+                        is Resource.Success -> {
+                            binding?.blogProgressBar?.visibility = View.GONE
+                            binding?.blogRecyclerView?.visibility = View.VISIBLE
+
+                            blogResult.data?.let { data -> blogAdapter.addCardItemList(data) }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private fun populateDiseaseList(diseaseAdapter: CardItemAdapter) {
-        val data = provideDummyDisease()
-        val diseaseData = mutableListOf<CommonItem>()
-        for (diseases in data) {
-            diseaseData.add(diseases.diseaseItem)
-        }
-        val diseasesFinal: List<CommonItem> = diseaseData
-        if (diseasesFinal.isNotEmpty()) {
-            diseaseAdapter.addCardItemList(diseasesFinal)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.CREATED) {
+                viewModel.diseaseResult.collectLatest { diseaseResult ->
+                    when (diseaseResult) {
+                        is Resource.Loading -> {
+                            binding?.diseaseProgressBar?.visibility = View.VISIBLE
+                        }
+
+                        is Resource.Error -> {
+                            binding?.diseaseProgressBar?.visibility = View.GONE
+                        }
+
+                        is Resource.Success -> {
+                            binding?.diseaseProgressBar?.visibility = View.GONE
+                            binding?.diseaseRecyclerView?.visibility = View.VISIBLE
+                            diseaseResult.data?.let { data ->
+                                val diseaseData = mutableListOf<CommonItem>()
+                                for (diseases in data.data!!) {
+                                    val realData = CommonItem(
+                                        id = diseases.dssId,
+                                        name = diseases.dssName,
+                                        image = diseases.dssImg,
+                                        desc = diseases.dssDesc
+                                    )
+                                    diseaseData.add(realData)
+                                }
+                                diseaseAdapter.addCardItemList(diseaseData)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
--- app/src/main/java/com/jeflette/epidermai/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/jeflette/epidermai/presentation/home/HomeViewModel.kt
@@ -1,7 +1,47 @@
 package com.jeflette.epidermai.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jeflette.epidermai.data.EpidermAIRepository
+import com.jeflette.epidermai.data.local.model.CommonItem
+import com.jeflette.epidermai.data.remote.responses.DiseasesResponse
+import com.jeflette.epidermai.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import provideBlog
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: EpidermAIRepository
+) : ViewModel(
+) {
+    private var _blogResult: MutableStateFlow<Resource<List<CommonItem>>> =
+        MutableStateFlow(Resource.Loading())
+
+    val blogResult = _blogResult.asStateFlow()
+
+    private var _diseaseResult: MutableStateFlow<Resource<DiseasesResponse?>> =
+        MutableStateFlow(Resource.Loading())
+
+    val diseaseResult = _diseaseResult.asStateFlow()
+
+
+    fun getAllDiseases() {
+        viewModelScope.launch {
+            repository.getAllDiseases().collectLatest {
+                _diseaseResult.value = it
+            }
+        }
+    }
+
+    fun getBlog() {
+        viewModelScope.launch {
+            _blogResult.value = Resource.Success(provideBlog())
+        }
+    }
+
 }
